b51d1cbcf01c6a7444329e1b38220370e59bce90
Hubert Lipiński <hubertlipinskipl@gmail.com>
2024-07-10 21:00:42 +0200

    wip: node-git status

--- src/commands/status.ts
+++ src/commands/status.ts
@@ -1,8 +1,14 @@
 import { getActiveBranch } from '../utils/branch'
 import { readIndex } from '../utils/gitIndex'
-import { repositoryHasChanges, resolveReference } from '../utils/repository'
+import { findObject, repositoryHasChanges, resolveReference } from '../utils/repository'
+import { getObjectDetails } from '../utils/filesystem'
+import { readTreeEntries } from '../utils/objects/tree'
 
 export default () => {
+  if (!repositoryHasChanges()) {
+    process.exit(0)
+  }
+
   // display the current branch
   const branch = getActiveBranch()
 
@@ -13,13 +19,21 @@ export default () => {
   }
 
   // changes to be committed
+  process.stdout.write('Changes to be committed:\n\n')
+  const index = readIndex()
+  const headTreeSha = findObject('HEAD', ObjectType.Tree)
+  const headDetails = getObjectDetails(headTreeSha!)
+  const tree = readTreeEntries(headDetails.content)
 
-  if (!repositoryHasChanges()) {
-    process.stdout.write('No commits yet\n\n')
-  } else {
-    process.stdout.write('Changes to be committed:\n\n')
-    const index = readIndex()
-    const head = resolveReference('HEAD')
+  process.stdout.write(`Tracked files: ${index.entries.length}\n`)
+  for (const item of index.entries) {
+    const headItem = tree.find((x) => x.filename === item.fileName)
+
+    if (!headItem) {
+      process.stdout.write(`  new file:   ${item.fileName}\n`)
+    } else if (headItem.hash !== item.hash) {
+      process.stdout.write(`  modified:   ${item.fileName}\n`)
+    }
   }
 
   // TODO: compare index with head
